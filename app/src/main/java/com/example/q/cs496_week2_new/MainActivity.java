23bfeacd3eac7ace1ccc4c22761f7c121d8c634d
LeeJunSeob <jsv1130@kaist.ac.kr>
2018-07-11 14:35:53 +0900

    changing facebook login procedure

--- app/src/main/java/com/example/q/cs496_week2_new/MainActivity.java
+++ app/src/main/java/com/example/q/cs496_week2_new/MainActivity.java
@@ -61,7 +61,7 @@ public class MainActivity extends AppCompatActivity {
         tabLayout.addOnTabSelectedListener(new TabLayout.ViewPagerOnTabSelectedListener(mViewPager));
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        /* fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 
@@ -70,7 +70,7 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(login);
                 finish();
             }
-        });
+        }); */
 
     }
 
